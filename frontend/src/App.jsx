2c0904a2152b639244fe646eebe857f10a28676e
michelangelo <mikybeeh@hotmail.it>
2026-01-10 17:49:52 +0100

    deleted 'bootstrap' package since react-bootstrap library is already installed; deleted bootstrap imports;

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
-//import { useState } from 'react';
 //import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Routes, Route } from 'react-router';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
