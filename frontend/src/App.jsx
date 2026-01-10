5d238a6a1e7620452ee8cc20db1ec038f887c9ba
michelangelo <mikybeeh@hotmail.it>
2026-01-10 18:44:15 +0100

    reverted previous commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 //import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Routes, Route } from 'react-router';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
